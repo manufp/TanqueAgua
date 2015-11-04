baa7d4f95f79c8b35b80e53681d8323071c77b9d
a022583435b <a022583435b@s09-pc18>
2015-11-04 20:56:41 +0100

    VERSION1

---
+++ src/net/ausiasmarch/tanque/modelo/EstadoTanque.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.ausiasmarch.tanque.modelo;
+
+/**
+ *
+ * @author a022583435b
+ */
+public enum EstadoTanque {
+    
+    LLENO, MEDIO, VACIO;
+    
+}
